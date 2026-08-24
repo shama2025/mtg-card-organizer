06f357462d84fd869ea081bd819a3196f2dfadef
shama2025 <2003mashaffer@gmail.com>
2026-08-24 15:28:36 -0400

    Create tests for fetchCardsFromCollection

---
+++ server/src/test/java/mtgcollection/data/CollectionJdbcRepositoryTest.java
@@ -0,0 +1,38 @@
+package mtgcollection.data;
+
+import mtgcollection.model.Card;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class CollectionJdbcRepositoryTest {
+
+    @Autowired
+    JdbcClient jdbcClient;
+
+    @Autowired
+    CollectionJdbcRepository repository;
+
+    @BeforeEach
+    void runSetKnownGoodState(){
+        jdbcClient.sql("call set_known_good_state();").update();
+    }
+
+    @Nested
+    class FetchAllCardsInUserCollectionTest{
+        @Test
+        void shouldFetchAllCardsForAGivenUser(){
+            List<Card> cardList = repository.fetchAllCards(1);
+            assertEquals(3, cardList.size());
+        }
+    }
+
+}
