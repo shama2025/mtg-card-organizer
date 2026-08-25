2c76309816c71981cac7bc2dae2b02b44d6a59c5
shama2025 <2003mashaffer@gmail.com>
2026-08-25 11:13:45 -0400

    Update Collection to remove cardId field

--- server/src/main/java/mtgcollection/model/Collection.java
+++ server/src/main/java/mtgcollection/model/Collection.java
@@ -5,12 +5,10 @@ import java.util.Objects;
 
 public class Collection {
     private int collectionId;
-    private List<Card> cardList;
     private int ownerId;
 
-    public Collection(int collectionId, List<Card> cardList, int ownerId) {
+    public Collection(int collectionId, int ownerId) {
         this.collectionId = collectionId;
-        this.cardList = cardList;
         this.ownerId = ownerId;
     }
 
@@ -20,14 +18,6 @@ public class Collection {
 
     public void setCollectionId(int collectionId) {
         this.collectionId = collectionId;
-    }
-
-    public List<Card> getCardList() {
-        return cardList;
-    }
-
-    public void setCardList(List<Card> cardList) {
-        this.cardList = cardList;
     }
 
     public int getOwner() {
@@ -42,11 +32,11 @@ public class Collection {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Collection that = (Collection) o;
-        return collectionId == that.collectionId && Objects.equals(cardList, that.cardList) && Objects.equals(ownerId, that.ownerId);
+        return collectionId == that.collectionId && Objects.equals(ownerId, that.ownerId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(collectionId, cardList, ownerId);
+        return Objects.hash(collectionId, ownerId);
     }
 }
