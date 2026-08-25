71ff84b28efd12c7b1def2410d93766ff194631e
shama2025 <2003mashaffer@gmail.com>
2026-08-24 20:48:19 -0400

    Implement tests for adding a addCard jdbc method

--- server/src/test/java/mtgcollection/data/CardJdbcRepositoryTest.java
+++ server/src/test/java/mtgcollection/data/CardJdbcRepositoryTest.java
@@ -1,11 +1,14 @@
 package mtgcollection.data;
 
+import jakarta.validation.constraints.Null;
+import mtgcollection.TestHelper;
 import mtgcollection.model.Card;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.simple.JdbcClient;
 
 import java.util.List;
@@ -33,6 +36,107 @@ class CardJdbcRepositoryTest {
             List<Card> cardList = repository.fetchAllCards(1);
             assertEquals(3, cardList.size());
         }
+    }
+
+    @Nested
+    class AddCardTests{
+        // Should Add Card
+        @Test
+        void shouldAddCardWithPopulatedValues(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            Card createdCard = repository.addCard(cardToAdd);
+            createdCard.setId(5);
+            assertEquals(cardToAdd,createdCard);
+        }
+
+        // Should add card with empty manaColor
+        @Test
+        void shouldAddCardWithEmptyManaColor(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            cardToAdd.setManaColor(null);
+            Card createdCard = repository.addCard(cardToAdd);
+            createdCard.setId(5);
+            assertEquals(cardToAdd,createdCard);
+        }
+
+        // Should not add null artist name
+        @Test
+        void shouldNotAddCardWithNullArtist(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            cardToAdd.setArtistName(null);
+            assertThrows(DataIntegrityViolationException.class,() ->{
+                repository.addCard(cardToAdd);
+            });
+
+        }
+
+        // Should not add card with null legalities
+        @Test
+        void shouldNotAddCardWithNullLegalities(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            cardToAdd.setLegalities(null);
+            assertThrows(NullPointerException.class,() ->{
+                repository.addCard(cardToAdd);
+            });
+
+        }
+
+        // Should not add card with null set
+        @Test
+        void shouldNotAddCardWithNullSet(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            cardToAdd.setSet(null);
+            assertThrows(NullPointerException.class,() ->{
+                repository.addCard(cardToAdd);
+            });
+
+        }
+
+
+        // Should not add card with null image path
+        @Test
+        void shouldNotAddCardWithNullImagePath(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            cardToAdd.setImgPath(null);
+            assertThrows(DataIntegrityViolationException.class,() ->{
+                repository.addCard(cardToAdd);
+            });
+
+        }
+
+        // Should not add card if name is null
+        @Test
+        void shouldNotAddCardWithNullName(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            cardToAdd.setName(null);
+            assertThrows(DataIntegrityViolationException.class,() ->{
+                repository.addCard(cardToAdd);
+            });
+
+        }
+
+        // Should Not add card if uuid is null
+        @Test
+        void shouldNotAddCardWithNullUUID(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            cardToAdd.setCardId(null);
+            assertThrows(NullPointerException.class,() ->{
+                repository.addCard(cardToAdd);
+            });
+
+        }
+
+        // Should Not add Card with null ManaCost
+        @Test
+        void shouldNotAddCardWithNullManaCost(){
+            Card cardToAdd = TestHelper.lightningBolt();
+            cardToAdd.setManaCost(null);
+            assertThrows(NullPointerException.class,() ->{
+                repository.addCard(cardToAdd);
+            });
+
+        }
+
     }
 
 }
