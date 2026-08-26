536353a5776232e0a6f16e250444a0f2151c207a
shama2025 <2003mashaffer@gmail.com>
2026-08-25 21:18:16 -0400

    Add card creation endpoint

--- server/src/main/java/mtgcollection/controller/CollectionController.java
+++ server/src/main/java/mtgcollection/controller/CollectionController.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import mtgcollection.domain.CollectionService;
 import mtgcollection.dto.LoggedInUser;
 import mtgcollection.model.Collection;
+import mtgcollection.model.Result;
+import mtgcollection.model.card.Card;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -36,5 +38,26 @@ public class CollectionController {
         LoggedInUser user = mapper.readValue(userAuthJson,LoggedInUser.class);
         Collection collection = collectionService.findCollectionByUserId(user.id());
         return new ResponseEntity<>(collectionService.fetchAllCardsByCollection(collection.getCollectionId()),HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addCardToCollection(@RequestHeader Map<String,String> headers, @RequestBody String cardName) throws JsonProcessingException, InterruptedException {
+        if(headers.get("authorization") == null){
+            // Missing auth header
+            return new ResponseEntity<>(List.of("Missing authorization header"), HttpStatus.BAD_REQUEST);
+        }
+        // Extract json from string
+        String userAuthJson = headers.get("authorization");
+        ObjectMapper mapper = new ObjectMapper();
+        LoggedInUser user = mapper.readValue(userAuthJson,LoggedInUser.class);
+        Collection collection = collectionService.findCollectionByUserId(user.id());
+        if(collection == null){
+            return new ResponseEntity<>(List.of("Collection does not correspond to user"),HttpStatus.NOT_FOUND);
+        }
+        Result<Card> result = collectionService.addCardToCollection(cardName,collection);
+        if(result.isSuccess()){
+            return new ResponseEntity<>(result.getpayload(),HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
     }
 }
