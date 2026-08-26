0f29cd3a08d69fb3500d721d03698c65b23ba0ed
shama2025 <2003mashaffer@gmail.com>
2026-08-26 16:11:36 -0400

    Implement fetchDeckByDeckId service method

--- server/src/main/java/mtgcollection/domain/DeckService.java
+++ server/src/main/java/mtgcollection/domain/DeckService.java
@@ -2,6 +2,8 @@ package mtgcollection.domain;
 
 import mtgcollection.data.interfaces.DeckRepository;
 import mtgcollection.model.Deck;
+import mtgcollection.model.Result;
+import mtgcollection.model.ResultType;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,4 +18,15 @@ public class DeckService {
     }
 
     public List<Deck> fetchAllDecksByCollectionId(int collectionId){return deckRepository.fetchAllDecksInACollection(collectionId);}
+
+    public Result<Deck> fetchDeckByDeckId(int deckId){
+        Result<Deck> result = new Result<>();
+        Deck deck = deckRepository.fetchDeckByDeckId(deckId);
+        if(deck == null){
+            result.addErrorMessage("Deck not found.", ResultType.NOT_FOUND);
+        }else{
+            result.setpayload(deck);
+        }
+        return result;
+    }
 }
