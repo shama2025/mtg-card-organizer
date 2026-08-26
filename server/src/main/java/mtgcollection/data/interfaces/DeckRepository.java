865179bd8a310b32b170f1bbefbf019ddf54f27d
shama2025 <2003mashaffer@gmail.com>
2026-08-26 15:57:45 -0400

    Implement fetchDeckByDeckID repo method

--- server/src/main/java/mtgcollection/data/interfaces/DeckRepository.java
+++ server/src/main/java/mtgcollection/data/interfaces/DeckRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface DeckRepository {
 
     List<Deck> fetchAllDecksInACollection(int collectionId);
+
+    Deck fetchDeckByDeckId(int deckId);
 }
--- server/src/main/java/mtgcollection/data/jdbc/DeckJdbcRepository.java
+++ server/src/main/java/mtgcollection/data/jdbc/DeckJdbcRepository.java
@@ -30,4 +30,19 @@ public class DeckJdbcRepository implements DeckRepository {
                 .query(new DeckMapper())
                 .list();
     }
+
+    @Override
+    public Deck fetchDeckByDeckId(int deckId) {
+        final String sql = """
+                select d.deck_id ,d.name ,d.date_created ,d.date_updated, sum(cd.quantity) as card_count from deck d
+                inner join collection_deck cd2 on cd2.deck_id = d.deck_id
+                left join card_deck cd on cd.deck_id = d.deck_id
+                where d.deck_id = :deckId
+                group by d.deck_id, d.name, d.date_created, d.date_updated;
+                """;
+        return jdbcClient.sql(sql)
+                .param("deckId", deckId)
+                .query(new DeckMapper())
+                .single();
+    }
 }
