2ef1932d83969eea97fef1293b0a30fa701d0555
shama2025 <2003mashaffer@gmail.com>
2026-08-26 13:12:43 -0400

    Update sql schema to include quantity field in card_deck bridge table

--- server/database/test.sql
+++ server/database/test.sql
@@ -39,10 +39,12 @@ create table if not EXISTS deck(
 create table if not exists card_deck(
     card_id int not null,
     deck_id int not null,
-    constraint fk_deck_deck
+    quantity int not null,
+    constraint chk_card_deck_quantity check(quantity >= 0),
+    constraint fk_deck_deck_id
     FOREIGN key(deck_id)
-    REFERENCES deck(deck_id),
-    constraint fk_deck_card
+    references deck(deck_id),
+    constraint fk_deck_card_id
     FOREIGN key(card_id)
     references card(card_id)
 );
@@ -56,17 +58,17 @@ create table if not EXISTS collection (
 );
 	
 create table if not exists collection_card(
-	collection_id int not null,
-	card_id int not NULL,
-	quantity int not null,
-	constraint chk_card_quantity check(quantity>=0),
-	constraint fk_collection_collection_id
-	foreign key(collection_id)
-	references collection(collection_id),
-	constraint fk_collection_user_id
-	FOREIGN key(card_id)
-	REFERENCES card(card_id)
-	);
+    collection_id int not null,
+    card_id int not NULL,
+    quantity int not null,
+    constraint chk_card_quantity check(quantity >= 0),
+    constraint fk_collection_collection_id
+    foreign key(collection_id)
+    references collection(collection_id),
+    constraint fk_collection_user_id
+    FOREIGN key(card_id)
+    REFERENCES card(card_id)
+);
 
 DELIMITER //
 
@@ -74,7 +76,6 @@ DROP PROCEDURE IF EXISTS set_known_good_state //
 
 CREATE PROCEDURE set_known_good_state()
 BEGIN
-    -- Delete bridge/child tables first to satisfy foreign key constraints
     delete from collection_card;
     delete from collection;
     delete from card_deck;
@@ -94,12 +95,12 @@ BEGIN
         ('a@a.com', 'a'),
         ('b@b.com', 'b');
 
-    -- 2. Seed Cards (Includes JSON_OBJECT for img_path)
+    -- 2. Seed Cards
     insert into card (card_uuid, name, img_path, mana_color, mana_cost, sets, legalities, artist, quantity) values
         (
             'f29ba16f-c8fb-42fe-aabf-87089cb214a7',
             'LIGHTNING BOLT',
-            JSON_ARRAY(
+            JSON_OBJECT(
                 'small', 'https://cards.scryfall.io/small/front/f/2/f29ba16f-c8fb-42fe-aabf-87089cb214a7.jpg',
                 'normal', 'https://cards.scryfall.io/normal/front/f/2/f29ba16f-c8fb-42fe-aabf-87089cb214a7.jpg',
                 'large', 'https://cards.scryfall.io/large/front/f/2/f29ba16f-c8fb-42fe-aabf-87089cb214a7.jpg',
@@ -116,7 +117,7 @@ BEGIN
         (
             'a97f330d-6823-4337-a7d1-e9c522d08a5c',
             'COUNTERSPELL',
-            JSON_ARRAY(
+            JSON_OBJECT(
                 'small', 'https://cards.scryfall.io/small/front/a/9/a97f330d-6823-4337-a7d1-e9c522d08a5c.jpg',
                 'normal', 'https://cards.scryfall.io/normal/front/a/9/a97f330d-6823-4337-a7d1-e9c522d08a5c.jpg',
                 'large', 'https://cards.scryfall.io/large/front/a/9/a97f330d-6823-4337-a7d1-e9c522d08a5c.jpg',
@@ -133,7 +134,7 @@ BEGIN
         (
             'ab851e3a-7f61-464a-9520-21a4f02a3a10',
             'SOL RING',
-            JSON_ARRAY(
+            JSON_OBJECT(
                 'small', 'https://cards.scryfall.io/small/front/a/b/ab851e3a-7f61-464a-9520-21a4f02a3a10.jpg',
                 'normal', 'https://cards.scryfall.io/normal/front/a/b/ab851e3a-7f61-464a-9520-21a4f02a3a10.jpg',
                 'large', 'https://cards.scryfall.io/large/front/a/b/ab851e3a-7f61-464a-9520-21a4f02a3a10.jpg',
@@ -150,7 +151,7 @@ BEGIN
         (
             'fe0a79f6-e0b3-4705-894c-e83cb41c18ca',
             'BIRDS OF PARADISE',
-            JSON_ARRAY(
+            JSON_OBJECT(
                 'small', 'https://cards.scryfall.io/small/front/f/e/fe0a79f6-e0b3-4705-894c-e83cb41c18ca.jpg',
                 'normal', 'https://cards.scryfall.io/normal/front/f/e/fe0a79f6-e0b3-4705-894c-e83cb41c18ca.jpg',
                 'large', 'https://cards.scryfall.io/large/front/f/e/fe0a79f6-e0b3-4705-894c-e83cb41c18ca.jpg',
@@ -170,13 +171,13 @@ BEGIN
         ('Izzet Spellslinger', 60, '2026-08-01', '2026-08-20'),
         ('Mono Green Ramp', 100, '2026-08-15', '2026-08-24');
 
-    -- 4. Seed Card-Deck Bridge (Links cards to decks)
-    insert into card_deck (card_id, deck_id) values
-        (1, 1), -- Lightning Bolt -> Izzet Spellslinger
-        (2, 1), -- Counterspell -> Izzet Spellslinger
-        (3, 1), -- Sol Ring -> Izzet Spellslinger
-        (3, 2), -- Sol Ring -> Mono Green Ramp
-        (4, 2); -- Birds of Paradise -> Mono Green Ramp
+    -- 4. Seed Card-Deck Bridge (Links cards to decks with quantities)
+    insert into card_deck (card_id, deck_id, quantity) values
+        (1, 1, 4), -- 4x Lightning Bolt -> Izzet Spellslinger
+        (2, 1, 4), -- 4x Counterspell -> Izzet Spellslinger
+        (3, 1, 1), -- 1x Sol Ring -> Izzet Spellslinger
+        (3, 2, 1), -- 1x Sol Ring -> Mono Green Ramp
+        (4, 2, 4); -- 4x Birds of Paradise -> Mono Green Ramp
 
     -- 5. Seed Collections
     insert into collection (user_id) values
