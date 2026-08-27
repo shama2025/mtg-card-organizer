6e4cce21adbe966ab1ad34559d6f641558390f41
shama2025 <2003mashaffer@gmail.com>
2026-08-27 10:33:13 -0400

    Implement fetchAllCardDeck jdbc method

---
+++ server/src/main/java/mtgcollection/data/jdbc/CardDeckJdbcRepository.java
@@ -0,0 +1,30 @@
+package mtgcollection.data.jdbc;
+
+import mtgcollection.data.interfaces.CardDeckRepository;
+import mtgcollection.model.CardDeck;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CardDeckJdbcRepository implements CardDeckRepository {
+
+    private final JdbcClient jdbcClient;
+
+    public CardDeckJdbcRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    @Override
+    public List<CardDeck> fetchAllCardDecksFromDeckId(int deckId) {
+        final String sql = """
+                select cd.card_id, cd.deck_id, cd.quantity from card_deck cd
+                where cd.deck_id = :deckId;
+                """;
+        return jdbcClient.sql(sql)
+                .param("deckId",deckId)
+                .query(CardDeck.class)
+                .list();
+    }
+}
