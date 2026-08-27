defbddca8375f5c296b59855605dcca4a68229c3
shama2025 <2003mashaffer@gmail.com>
2026-08-27 10:29:48 -0400

    Update DeckService to include appending list of cards to a deck

--- server/src/main/java/mtgcollection/domain/DeckService.java
+++ server/src/main/java/mtgcollection/domain/DeckService.java
@@ -1,11 +1,16 @@
 package mtgcollection.domain;
 
+import mtgcollection.data.interfaces.CardDeckRepository;
+import mtgcollection.data.interfaces.CardRepository;
 import mtgcollection.data.interfaces.DeckRepository;
+import mtgcollection.model.CardDeck;
 import mtgcollection.model.Deck;
 import mtgcollection.model.Result;
 import mtgcollection.model.ResultType;
+import mtgcollection.model.card.Card;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,20 +18,33 @@ public class DeckService {
 
     private final DeckRepository deckRepository;
 
-    public DeckService(DeckRepository deckRepository) {
+    private final CardDeckRepository cardDeckRepository;
+
+    private final CardRepository cardRepository;
+
+    public DeckService(DeckRepository deckRepository,CardDeckRepository cardDeckRepository,CardRepository cardRepository) {
         this.deckRepository = deckRepository;
+        this.cardDeckRepository = cardDeckRepository;
+        this.cardRepository = cardRepository;
     }
 
     public List<Deck> fetchAllDecksByCollectionId(int collectionId){return deckRepository.fetchAllDecksInACollection(collectionId);}
 
     public Result<Deck> fetchDeckByDeckId(int deckId){
         Result<Deck> result = new Result<>();
+        List<Card> cardList = new ArrayList<>();
         Deck deck = deckRepository.fetchDeckByDeckId(deckId);
         if(deck == null){
             result.addErrorMessage("Deck not found.", ResultType.NOT_FOUND);
-        }else{
-            result.setpayload(deck);
+            return result;
         }
+        List<CardDeck> cardDeckList = cardDeckRepository.fetchAllCardDecksFromDeckId(deckId);
+        for(CardDeck cardDeck : cardDeckList){
+            Card card = cardRepository.fetchCardById(cardDeck.cardId());
+            cardList.add(card);
+        }
+        deck.setCardList(cardList);
+        result.setpayload(deck);
         return result;
     }
 }
