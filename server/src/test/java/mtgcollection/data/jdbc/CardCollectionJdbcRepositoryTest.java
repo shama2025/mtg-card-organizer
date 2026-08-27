858fce6c2a6406e038e55349553241964c446b63
shama2025 <2003mashaffer@gmail.com>
2026-08-27 09:08:20 -0400

    Update test to support asserting for Thrown error

--- server/src/test/java/mtgcollection/data/jdbc/CardCollectionJdbcRepositoryTest.java
+++ server/src/test/java/mtgcollection/data/jdbc/CardCollectionJdbcRepositoryTest.java
@@ -10,7 +10,10 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.jdbc.core.simple.JdbcClient;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -111,8 +114,9 @@ class CardCollectionJdbcRepositoryTest {
             int cardQuantity = -5;
             int collectionThatExists = TestHelper.collection().getCollectionId();
             int cardThatExists = TestHelper.lightningBolt().getId();
-            boolean isUpdated = cardCollectionJdbcRepository.updateCardInCollection(cardThatExists,collectionThatExists,cardQuantity);
-            assertFalse(isUpdated);
+            assertThrows(UncategorizedSQLException.class, () ->{
+                cardCollectionJdbcRepository.updateCardInCollection(cardThatExists,collectionThatExists,cardQuantity);
+            });
         }
     }
 
