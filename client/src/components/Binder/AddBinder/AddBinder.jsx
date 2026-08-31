0050edebca8dd92488d7ba70ed59f3cba3dbf900
shama2025 <2003mashaffer@gmail.com>
2026-08-31 11:00:59 -0400

    Refactor binder components

---
+++ client/src/components/Binder/AddBinder/AddBinder.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { SquarePlus } from "lucide-react";
+
+export default function AddBinder({ setDisplayBinderCardModal }) {
+  return (
+    <div className="flex flex-col">
+      <p className="text-jeskai-white-pure text-lg">Binders</p>
+      <div className="m-1 flex flex-row">
+        <p>Add a Binder</p>
+        <SquarePlus
+          className="relative m-1 left-1 bottom-1 
+        hover:scale-105 hover:cursor-pointer"
+          onClick={() => setDisplayBinderCardModal(false)}
+        />
+      </div>
+    </div>
+  );
+}
