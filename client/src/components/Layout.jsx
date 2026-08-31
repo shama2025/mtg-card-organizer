14077f5a67e2b9442da0e360f29869a27e6f47cd
shama2025 <2003mashaffer@gmail.com>
2026-08-31 10:51:51 -0400

    Update layout component to auto navigate to collection if user is logged in

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Nav from "./Navbar/Nav/Nav";
+import { useContext, useEffect } from "react";
+import { LoggedInUser } from "../contexts/LoggedInUser";
 
 export default function Layout() {
+
+  const loggedInUser = useContext(LoggedInUser)
+  const navigator = useNavigate()
+
+  useEffect(() => {
+    if(loggedInUser){
+      navigator("/collection");
+    }
+},[])
+
   return (
     <div className="bg-jeskai-dark h-full">
       <Nav />
