742e3334c295cd0947a4ec38c7931d60edf9f696
shama2025 <2003mashaffer@gmail.com>
2026-09-03 11:59:29 -0400

    Remove branding page context import

--- client/src/components/BrandingPage/BrandingPage.jsx
+++ client/src/components/BrandingPage/BrandingPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import mtgLogo from "../assets/mtg-logo.png";
 import parse from "html-react-parser";
 import { NavLink } from "react-router-dom";
-import { DisplayBrandingPage } from "../../contexts/DisplayBrandingPage";
 import { useContext } from "react";
 
 export default function BrandingPage() {
