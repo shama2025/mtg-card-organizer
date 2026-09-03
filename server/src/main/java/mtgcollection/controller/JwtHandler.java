864a80f78e3125be5c449d0e4e75bd005b016d46
shama2025 <2003mashaffer@gmail.com>
2026-09-03 10:39:00 -0400

    Create JwtHandler class

---
+++ server/src/main/java/mtgcollection/controller/JwtHandler.java
@@ -0,0 +1,45 @@
+package mtgcollection.controller;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtHandler {
+
+    private final String SECRET = System.getenv("SECRET");
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private final int EXPIRATION_MS = 86400000; // 1 day
+
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
